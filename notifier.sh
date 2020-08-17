04f1d31bc2bf4f9a1494b9933d26acb4e3bd9319
tanushbanerjee <samihanbanerjee026@gmail.com>
2020-08-17 20:16:34 +0530

    Initial Commit

---
+++ notifier.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true
+do
+	zenity --info --title "Please Drink Water now" --text "The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women."
+done
